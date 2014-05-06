8286d588d26fa2cc4fa3025ad2cfc3786c95b603
Rob van Bekkum <rob_van_bekkum@hotmail.com>
2014-05-06 14:39:53 +0200

    Implemented camera following the taxi in a map
    Adapted GameScreen.java to import a test map and following the taxi.
    Also adapted test_map.tmx a little bit to better test the following of
    the taxi.

--- taxi-test/core/src/com/mygdx/game/screens/GameScreen.java
+++ taxi-test/core/src/com/mygdx/game/screens/GameScreen.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
@@ -24,6 +27,8 @@ public class GameScreen extends BasicScreen implements Screen {
 	private Stage stage;
 	private InputMultiplexer multiplexer;
 	private TouchInputProcessor touchInput;
+	private TiledMap tileMap;
+	private OrthogonalTiledMapRenderer tmrenderer;
 
 	@Override
 	public void show() {
@@ -49,15 +54,15 @@ public class GameScreen extends BasicScreen implements Screen {
 		Vector2 center = new Vector2(worldWidth / 2, worldHeight / 2);
 
 		// pen in the center
-		BoxProp pen1 = new BoxProp(world, 1, 6, new Vector2(center.x - 3,
+		/*BoxProp pen1 = new BoxProp(world, 1, 6, new Vector2(center.x - 3,
 				center.y));
 		BoxProp pen2 = new BoxProp(world, 1, 6, new Vector2(center.x + 3,
 				center.y));
 		BoxProp pen3 = new BoxProp(world, 5, 1, new Vector2(center.x,
-				center.y + 2.5f));
+				center.y + 2.5f));*/
 
 		// outer walls
-		BoxProp wall1 = new BoxProp(world, worldWidth, 1, new Vector2(
+		/*BoxProp wall1 = new BoxProp(world, worldWidth, 1, new Vector2(
 				worldWidth / 2, 0.5f)); // bottom
 		BoxProp wall2 = new BoxProp(world, 1, worldHeight - 2, new Vector2(
 				0.5f, worldHeight / 2));// left
@@ -65,12 +70,19 @@ public class GameScreen extends BasicScreen implements Screen {
 				worldWidth / 2, worldHeight - 0.5f));// top
 		BoxProp wall4 = new BoxProp(world, 1, worldHeight - 2, new Vector2(
 				worldWidth - 0.5f, worldHeight / 2)); // right
+*/		
+		// load the map
+		tileMap = new TmxMapLoader().load("maps/test_map.tmx");
+		tmrenderer = new OrthogonalTiledMapRenderer(tileMap);
 	}
 
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+		
+		// set the camera to follow the taxi
+		camera.position.set(car.body.getPosition().x * PIXELS_PER_METER, car.body.getPosition().y * PIXELS_PER_METER,0);
 
 		// tell the camera to update its matrices.
 		camera.update();
@@ -102,6 +114,10 @@ public class GameScreen extends BasicScreen implements Screen {
 		world.step(Gdx.app.getGraphics().getDeltaTime(), 3, 3);
 
 		world.clearForces();
+		
+		//draw the map
+		tmrenderer.setView(camera);
+		tmrenderer.render();
 
 		/**
 		 * Draw this last, so we can see the collision boundaries on top of the
