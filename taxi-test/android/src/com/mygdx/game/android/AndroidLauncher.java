7f4475b4721cbdf21271078b2f827fb705cce64b
Joost vanOorschot <joost94@gmail.com>
2014-05-06 11:36:29 +0200

    First try at structuring the project.

--- taxi-test/android/src/com/mygdx/game/android/AndroidLauncher.java
+++ taxi-test/android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.TopDownCar;
+import com.mygdx.game.TaxiTest;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new TopDownCar(), config);
+		initialize(new TaxiTest(), config);
 	}
 }
---
+++ taxi-test/core/src/com/mygdx/game/screens/BasicScreen.java
@@ -0,0 +1,11 @@
+package com.mygdx.game.screens;
+
+import properties.GameProperties;
+
+public class BasicScreen {
+	protected int screenWidth = GameProperties.screenWidth;
+	protected int screenHeight = GameProperties.screenHeight;
+	protected float worldWidth = GameProperties.worldWidth;
+	protected float worldHeight = GameProperties.worldHeight;
+	protected static int PIXELS_PER_METER = GameProperties.PIXELS_PER_METER;
+}
