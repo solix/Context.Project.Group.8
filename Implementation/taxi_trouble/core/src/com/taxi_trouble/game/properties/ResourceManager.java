a8406e1e6e5ef1bc9711cb0145cbe09ea86436b0
solix <soheil.jahanshahi@gmail.com>
2014-05-13 22:47:39 +0200

    testing if jenkins reads gradle build on root directory

--- Implementation/taxi_trouble/core/src/com/taxi_trouble/game/properties/ResourceManager.java
+++ Implementation/taxi_trouble/core/src/com/taxi_trouble/game/properties/ResourceManager.java
@@ -4,4 +4,5 @@ public class ResourceManager {
 	public static String mapFile = "maps/prototype.tmx";
 	public static String carSprite = "sprites/taxi_frame.png";
 	public static String wheelSprite = "sprites/wheel.png";
+	
 }
