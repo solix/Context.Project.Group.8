eaf743d378d5af4f9b41e807dc947bd47449ed0f
solix <soheil.jahanshahi@gmail.com>
2014-05-14 22:26:49 +0200

    added SoundBank.java and asset/audio

---
+++ taxi-test/core/src/com/mygdx/game/sound/SoundBank.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.sound;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
+/**
+ * 
+ * @author 5oheil
+ * 
+ */
+public class SoundBank {
+	public static Sound testSound;
+
+	public static void load() {
+		testSound = Gdx.audio.newSound(Gdx.files.internal("audio1.Wav"));
+	}
+}
