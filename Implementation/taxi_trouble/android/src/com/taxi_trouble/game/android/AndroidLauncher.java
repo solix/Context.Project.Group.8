bc8c311b98c7f6c576aa0f1445d80b6e88b63501
Joost vanOorschot <joost94@gmail.com>
2014-05-20 11:51:12 +0200

    Added user login screen

--- Implementation/taxi_trouble/android/src/com/taxi_trouble/game/android/AndroidLauncher.java
+++ Implementation/taxi_trouble/android/src/com/taxi_trouble/game/android/AndroidLauncher.java
@@ -1,55 +1,14 @@
 package com.taxi_trouble.game.android;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.taxi_trouble.game.TaxiTest;
-import com.taxi_trouble.game.gdxGooglePlay.GooglePlayInterface;
-import com.taxi_trouble.game.model.GameWorld;
-import com.google.example.games.basegameutils.
-
-public class AndroidLauncher extends AndroidApplication implements GooglePlayInterface {
+public class AndroidLauncher extends Activity {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new GameWorld(this), config);
-	}
-
-	@Override
-	public void Login() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void LogOut() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean getSignedIn() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void submitScore(int score) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void getScores() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void getScoresData() {
-		// TODO Auto-generated method stub
-		
+		Intent loginIntent = new Intent(this, LoginActivity.class);
+		startActivity(loginIntent);
 	}
 }
