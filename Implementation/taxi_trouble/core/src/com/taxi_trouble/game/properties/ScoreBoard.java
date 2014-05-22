7f4575839c2361a4805353eb79e28fc142aabb6f
solix <soheil.jahanshahi@gmail.com>
2014-05-22 16:20:21 +0200

    added score board to master branch

---
+++ Implementation/taxi_trouble/core/src/com/taxi_trouble/game/properties/ScoreBoard.java
@@ -0,0 +1,31 @@
+package com.taxi_trouble.game.properties;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class ScoreBoard{
+
+	private static int score;
+	private static BitmapFont scoreFont;
+	private static String scoreName;
+	
+	public void createScoreBoard(){
+		score=0;
+		scoreName="Score  0";
+		scoreFont=new BitmapFont();
+		
+	}
+	
+	public void render(){
+		SpriteBatch scoreBatch=new SpriteBatch();
+		scoreBatch.begin();
+		scoreFont.setColor(0.0f, 0.0f, 1.0f, 1.0f);
+		scoreFont.draw(scoreBatch, scoreName, 10, GameProperties.screenHeight -10);
+		scoreBatch.end();  
+		
+	}
+	
+
+}
