0a70219a330fed9d36a8cb114b8a59ffbc05bbff
solix <soheil.jahanshahi@gmail.com>
2014-05-26 19:18:36 +0200

    [Jenkins]#1 attempt Junit 4.12

---
+++ Implementation/taxi_trouble/core/test/PassangerTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+
+public class PassangerTest {
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPassenger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTransporter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResetSpawnPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDestination() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRender() {
+		fail("Not yet implemented");
+	}
+
+}
