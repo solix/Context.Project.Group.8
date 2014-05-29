3851743ad30158d3353704f1dfbf7feda1eaf933
Rob van Bekkum <rob_van_bekkum@hotmail.com>
2014-05-29 23:30:36 +0200

    Added CollisionDetectorTest
    Refactored CollisionDetector class (functionality stayed exactly the
    same)

---
+++ Implementation/taxi_trouble/core/test/com/taxi_trouble/game/model/CollisionDetectorTest.java
@@ -0,0 +1,81 @@
+package com.taxi_trouble.game.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**This class tests the functionality of the Passenger class.
+*
+* @author Computer Games Project Group 8
+*
+*/
+@RunWith(MockitoJUnitRunner.class)
+public class CollisionDetectorTest {
+
+    private CollisionDetector collisionDetector;
+
+    @Mock
+    private Taxi taxi;
+
+    @Mock
+    private Taxi secondTaxi;
+
+    @Mock
+    private Passenger passenger;
+
+    @Mock
+    private Destination destination;
+
+    @Mock
+    private WorldMap map;
+
+    /**Initializes the collision detector that should be used to test
+     * the different types of collisions that may occur.
+     *
+     */
+    @Before
+    public final void setup() {
+        collisionDetector = new CollisionDetector(map);
+    }
+
+    /**Check that when a taxi drives into a passenger, the passenger
+     * is picked up by the taxi (if this applies, i.e. if the taxi
+     * does not have a passenger yet).
+     *
+     */
+    @Test
+    public final void collideEmptyTaxiWithPassenger() {
+        collisionDetector.collide(taxi, passenger);
+        verify(taxi).pickUpPassenger(passenger);
+    }
+
+    /**Check that when a taxi drives into a destination, the taxi will
+     * drop off its passenger (if this applies, i.e. if the taxi has
+     * a passenger and the destination is right)
+     *
+     */
+    @Test
+    public final void collideTaxiWithDestination() {
+        collisionDetector.collide(taxi, destination);
+        taxi.dropOffPassenger(destination, map);
+    }
+
+    /**Check that when a taxi drives into another taxi, the taxi will
+     * steal the passenger from the other passenger (if this applies).
+     *
+     */
+    @Test
+    public final void collideTaxiWithOtherTaxi() {
+        collisionDetector.collide(taxi, secondTaxi);
+        taxi.stealPassenger(secondTaxi);
+    }
+}
