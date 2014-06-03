d77f29d0dd589f6974d9259a0d78178e73e6730b
Rob van Bekkum <rob_van_bekkum@hotmail.com>
2014-06-03 16:31:01 +0200

    Removed taxi-test folder (no longer used)

--- taxi-test/core/src/com/taxitrouble/game/properties/SpriteProperties.java
+++
@@ -1,7 +0,0 @@
-package com.taxitrouble.game.properties;
-
-public class SpriteProperties {
-	public static String throttleSprite = "sprites/throttle.png";
-	public static String breakSprite = "sprites/break.png";
-	public static String dPadSprite = "sprites/dpad.png";
-}
