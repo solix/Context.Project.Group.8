44e9cb1b2fc6c2a46199f6b71cefe49abdc9ee2f
Rob van Bekkum <rob_van_bekkum@hotmail.com>
2014-06-05 00:08:12 +0200

    Small change for drawing Destinations in the citymap
    Drawing the sprite of the destination specified by destination.png

--- Implementation/taxi_trouble/core/src/com/taxi_trouble/game/model/Destination.java
+++ Implementation/taxi_trouble/core/src/com/taxi_trouble/game/model/Destination.java
@@ -147,8 +147,6 @@ public class Destination {
         spriteBatch.begin();
         destinationSprite.setPosition(this.getXPosition() * PIXELS_PER_METER,
                 this.getYPosition() * PIXELS_PER_METER);
-        destinationSprite.setRegionHeight((int) this.getHeight());
-        destinationSprite.setRegionWidth((int) this.getWidth());
         destinationSprite.setScale(PIXELS_PER_METER);
         destinationSprite.draw(spriteBatch);
         spriteBatch.end();
