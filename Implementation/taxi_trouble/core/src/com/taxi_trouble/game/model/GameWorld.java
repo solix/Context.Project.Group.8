7c11e69034a6c3eab0c1be33f2d5228ca534d302
ACAMauricio <A.C.A.mauricio@student.tudelft.nl>
2014-06-20 10:36:09 +0200

    The game waits 5 seconds before resetting now

--- Implementation/taxi_trouble/core/src/com/taxi_trouble/game/model/GameWorld.java
+++ Implementation/taxi_trouble/core/src/com/taxi_trouble/game/model/GameWorld.java
@@ -27,279 +27,279 @@ import com.taxi_trouble.game.screens.NavigatorScreen;
  * 
  */
 public class GameWorld extends Game {
-	private WorldMap map;
-	private Team ownTeam;
-	private Map<Integer, Team> teams;
-	private AndroidMultiplayerInterface multiplayerInterface;
-	private SetupInterface setupInterface;
-	private DriverScreen driverScreen;
-	private NavigatorScreen navigatorScreen;
-	private MenuScreen menuScreen;
-	private CountDownTimer timer;
-	private boolean isActive;
+    private WorldMap map;
+    private Team ownTeam;
+    private Map<Integer, Team> teams;
+    private AndroidMultiplayerInterface multiplayerInterface;
+    private SetupInterface setupInterface;
+    private DriverScreen driverScreen;
+    private NavigatorScreen navigatorScreen;
+    private MenuScreen menuScreen;
+    private CountDownTimer timer;
+    private boolean isActive;
 
-	/**
-	 * Initializes a new game world in which a game can be played.
-	 * 
-	 * @param setupInterface
-	 * @param multiplayerInterface
-	 */
-	public GameWorld(SetupInterface setupInterface,
-			AndroidMultiplayerInterface multiplayerInterface) {
-		this.setupInterface = setupInterface;
-		this.multiplayerInterface = multiplayerInterface;
-		this.teams = new HashMap<Integer, Team>();
-	}
+    /**
+     * Initializes a new game world in which a game can be played.
+     * 
+     * @param setupInterface
+     * @param multiplayerInterface
+     */
+    public GameWorld(SetupInterface setupInterface,
+            AndroidMultiplayerInterface multiplayerInterface) {
+        this.setupInterface = setupInterface;
+        this.multiplayerInterface = multiplayerInterface;
+        this.teams = new HashMap<Integer, Team>();
+    }
 
-	/**
-	 * Called when the game world is first created.
-	 * 
-	 */
-	@Override
-	public final void create() {
-		loadResources();
-		World world = new World(new Vector2(0.0f, 0.0f), true);
-		map = new WorldMap(getTiledMap(), world);
-		CollisionDetector collisionDetector = new CollisionDetector(map);
-		world.setContactListener(collisionDetector);
-		collisionDetector.setMultiPlayerInterface(multiplayerInterface);
-		map.setMultiPlayerInterface(multiplayerInterface);
-		timer = new CountDownTimer(300);
-		setActive(false);
+    /**
+     * Called when the game world is first created.
+     * 
+     */
+    @Override
+    public final void create() {
+        loadResources();
+        World world = new World(new Vector2(0.0f, 0.0f), true);
+        map = new WorldMap(getTiledMap(), world);
+        CollisionDetector collisionDetector = new CollisionDetector(map);
+        world.setContactListener(collisionDetector);
+        collisionDetector.setMultiPlayerInterface(multiplayerInterface);
+        map.setMultiPlayerInterface(multiplayerInterface);
+        timer = new CountDownTimer(300);
+        setActive(false);
 
-		driverScreen = new DriverScreen(this);
-		navigatorScreen = new NavigatorScreen(this);
-		menuScreen = new MenuScreen(setupInterface);
-		setScreen(menuScreen);
-	}
+        driverScreen = new DriverScreen(this);
+        navigatorScreen = new NavigatorScreen(this);
+        menuScreen = new MenuScreen(setupInterface);
+        setScreen(menuScreen);
+    }
 
-	@Override
-	public void resume() {
-		loadResources();
-	}
+    @Override
+    public void resume() {
+        loadResources();
+    }
 
-	@Override
-	public final void render() {
-		super.render();
-		if (isHost()) {
-			// Spawn a new passenger/power-up if there are less than #taxis-1.
-			Collection<Passenger> passengers = getPassengers();
-			if (passengers.size() < teams.size() || passengers.size() == 0) {
-				getSpawner().spawnPassenger(getWorld());
-			}
+    @Override
+    public final void render() {
+        super.render();
+        if (isHost()) {
+            // Spawn a new passenger/power-up if there are less than #taxis-1.
+            Collection<Passenger> passengers = getPassengers();
+            if (passengers.size() < teams.size() || passengers.size() == 0) {
+                getSpawner().spawnPassenger(getWorld());
+            }
 
-			Collection<PowerUp> powerups = getPowerUps();
-			if (powerups.size() < 3) {
-				getSpawner().spawnPowerUp(getWorld());
-			}
-		}
-	}
+            Collection<PowerUp> powerups = getPowerUps();
+            if (powerups.size() < 3) {
+                getSpawner().spawnPowerUp(getWorld());
+            }
+        }
+    }
 
-	/**
-	 * Starts the game by starting the timer and switching to the right screen.
-	 */
-	public final void startGame() {
-		timer.startTimer();
-		setScreen();
-	}
+    /**
+     * Starts the game by starting the timer and switching to the right screen.
+     */
+    public final void startGame() {
+        timer.startTimer();
+        setScreen();
+    }
 
-	/**
-	 * Sets whether the player is a driver or not.
-	 * 
-	 * @param driver
-	 *            : boolean indicating whether the player is a driver
-	 */
-	public void setDriver(boolean driver) {
-		multiplayerInterface.setDriver(driver);
-	}
+    /**
+     * Sets whether the player is a driver or not.
+     * 
+     * @param driver
+     *            : boolean indicating whether the player is a driver
+     */
+    public void setDriver(boolean driver) {
+        multiplayerInterface.setDriver(driver);
+    }
 
-	/**
-	 * Retrieves whether the player is a driver.
-	 * 
-	 * @return true if the player is a driver, false otherwise (a navigator)
-	 */
-	public boolean isDriver() {
-		return multiplayerInterface.isDriver();
-	}
+    /**
+     * Retrieves whether the player is a driver.
+     * 
+     * @return true if the player is a driver, false otherwise (a navigator)
+     */
+    public boolean isDriver() {
+        return multiplayerInterface.isDriver();
+    }
 
-	/**
-	 * Sets the assigned view. The assigned view of a player will be either the
-	 * DriverScreen or NavigatorScreen.
-	 */
-	public void setScreen() {
-		if (isDriver()) {
-			setScreen(driverScreen);
-		} else {
-			setScreen(navigatorScreen);
-		}
-	}
+    /**
+     * Sets the assigned view. The assigned view of a player will be either the
+     * DriverScreen or NavigatorScreen.
+     */
+    public void setScreen() {
+        if (isDriver()) {
+            setScreen(driverScreen);
+        } else {
+            setScreen(navigatorScreen);
+        }
+    }
 
-	/**
-	 * Brings the player back to the menu screen.
-	 */
-	public void returnToMenu() {
-		setScreen(menuScreen);
-	}
+    /**
+     * Brings the player back to the menu screen.
+     */
+    public void returnToMenu() {
+        setScreen(menuScreen);
+    }
 
-	/**
-	 * Retrieves the game world map.
-	 * 
-	 * @return map
-	 */
-	public WorldMap getMap() {
-		return this.map;
-	}
+    /**
+     * Retrieves the game world map.
+     * 
+     * @return map
+     */
+    public WorldMap getMap() {
+        return this.map;
+    }
 
-	/**
-	 * Retrieves the spawner that is used to spawn entities.
-	 * 
-	 * @return spawner
-	 */
-	public final Spawner getSpawner() {
-		return this.map.getSpawner();
-	}
+    /**
+     * Retrieves the spawner that is used to spawn entities.
+     * 
+     * @return spawner
+     */
+    public final Spawner getSpawner() {
+        return this.map.getSpawner();
+    }
 
-	/**
-	 * Retrieves the single team.
-	 * 
-	 * @return team
-	 */
-	public final Team getTeam() {
-		return this.ownTeam;
-	}
+    /**
+     * Retrieves the single team.
+     * 
+     * @return team
+     */
+    public final Team getTeam() {
+        return this.ownTeam;
+    }
 
-	/**
-	 * Retrieves the world in which the game is played.
-	 * 
-	 * @return world
-	 */
-	public final World getWorld() {
-		return map.getWorld();
-	}
+    /**
+     * Retrieves the world in which the game is played.
+     * 
+     * @return world
+     */
+    public final World getWorld() {
+        return map.getWorld();
+    }
 
-	/**
-	 * Retrieves the passengers that are currently in the game.
-	 * 
-	 * @return passengers
-	 */
-	public final Collection<Passenger> getPassengers() {
-		return getSpawner().getActivePassengers().values();
-	}
+    /**
+     * Retrieves the passengers that are currently in the game.
+     * 
+     * @return passengers
+     */
+    public final Collection<Passenger> getPassengers() {
+        return getSpawner().getActivePassengers().values();
+    }
 
-	/**
-	 * Sets up the teams that join the game.
-	 * 
-	 * @param teamId
-	 *            : the own team identifier
-	 * @param totalTeams
-	 *            : the total number of teams - 1
-	 */
-	public void setTeams(int teamId, int totalTeams) {
-		for (int i = 0; i < totalTeams; i++) {
-			Team team = new Team(i, getSpawner().spawnTaxi(getWorld(), i));
-			if (i == teamId) {
-				this.ownTeam = team;
-			}
-			teams.put(i, team);
-		}
-	}
+    /**
+     * Sets up the teams that join the game.
+     * 
+     * @param teamId
+     *            : the own team identifier
+     * @param totalTeams
+     *            : the total number of teams - 1
+     */
+    public void setTeams(int teamId, int totalTeams) {
+        for (int i = 0; i < totalTeams; i++) {
+            Team team = new Team(i, getSpawner().spawnTaxi(getWorld(), i));
+            if (i == teamId) {
+                this.ownTeam = team;
+            }
+            teams.put(i, team);
+        }
+    }
 
-	/**
-	 * Retrieves all teams that have joined the team.
-	 * 
-	 * @return teams
-	 */
-	public Map<Integer, Team> getTeams() {
-		return teams;
-	}
+    /**
+     * Retrieves all teams that have joined the team.
+     * 
+     * @return teams
+     */
+    public Map<Integer, Team> getTeams() {
+        return teams;
+    }
 
-	public AndroidMultiplayerInterface getMultiplayerInterface() {
-		return multiplayerInterface;
-	}
+    public AndroidMultiplayerInterface getMultiplayerInterface() {
+        return multiplayerInterface;
+    }
 
-	public void setHost(boolean host) {
-		this.multiplayerInterface.setHost(host);
-	}
+    public void setHost(boolean host) {
+        this.multiplayerInterface.setHost(host);
+    }
 
-	public boolean isHost() {
-		return this.multiplayerInterface.isHost();
-	}
+    public boolean isHost() {
+        return this.multiplayerInterface.isHost();
+    }
 
-	public Passenger getPassengerById(int id) {
-		return getSpawner().getActivePassengers().get(id);
-	}
+    public Passenger getPassengerById(int id) {
+        return getSpawner().getActivePassengers().get(id);
+    }
 
-	public Taxi getTaxiById(int id) {
-		return getTeamById(id).getTaxi();
-	}
+    public Taxi getTaxiById(int id) {
+        return getTeamById(id).getTaxi();
+    }
 
-	public Team getTeamById(int id) {
-		return getTeams().get(id);
-	}
+    public Team getTeamById(int id) {
+        return getTeams().get(id);
+    }
 
-	public PowerUp getPowerUpById(int id) {
-		return getSpawner().getActivePowerUps().get(id);
-	}
+    public PowerUp getPowerUpById(int id) {
+        return getSpawner().getActivePowerUps().get(id);
+    }
 
-	/**
-	 * Retrieves all currently available power-ups.
-	 * 
-	 * @return power-ups
-	 */
-	public final Collection<PowerUp> getPowerUps() {
-		return getSpawner().getActivePowerUps().values();
-	}
+    /**
+     * Retrieves all currently available power-ups.
+     * 
+     * @return power-ups
+     */
+    public final Collection<PowerUp> getPowerUps() {
+        return getSpawner().getActivePowerUps().values();
+    }
 
-	/**
-	 * Retrieves the game countdown-timer.
-	 * 
-	 * @return timer
-	 */
-	public final CountDownTimer getTimer() {
-		return this.timer;
-	}
+    /**
+     * Retrieves the game countdown-timer.
+     * 
+     * @return timer
+     */
+    public final CountDownTimer getTimer() {
+        return this.timer;
+    }
 
-	/**
-	 * Retrieves the winning team of a game.
-	 * 
-	 * @return winner
-	 */
-	public Team getWinner() {
-		Team team = null;
-		int highscore = -1;
-		for (Team t : teams.values()) {
-			if (t.getScore() > highscore) {
-				team = t;
-				highscore = t.getScore();
-			}
-		}
-		return team;
-	}
+    /**
+     * Retrieves the winning team of a game.
+     * 
+     * @return winner
+     */
+    public Team getWinner() {
+        Team team = null;
+        int highscore = -1;
+        for (Team t : teams.values()) {
+            if (t.getScore() > highscore) {
+                team = t;
+                highscore = t.getScore();
+            }
+        }
+        return team;
+    }
 
-	public void reset() {
-		System.out.println("resetting gameworld!!!");
-		setScreen(menuScreen);
-		setHost(false);
-		setupInterface.disconnect();
-		driverScreen = new DriverScreen(this);
-		navigatorScreen = new NavigatorScreen(this);
-		getSpawner().getActivePassengers().clear();
-		getSpawner().getActivePowerUps().clear();
-		for (Team team : getTeams().values()) {
-			team.getTaxi().removeBodyFromWorld(getWorld());
-		}
-		this.teams.clear();
-		timer = new CountDownTimer(300);
-		System.out.println("resetting done!");
+    public void reset() {
+        System.out.println("resetting gameworld!!!");
+        setScreen(menuScreen);
+        setHost(false);
+        setupInterface.disconnect();
+        driverScreen = new DriverScreen(this);
+        navigatorScreen = new NavigatorScreen(this);
+        getSpawner().getActivePassengers().clear();
+        getSpawner().getActivePowerUps().clear();
+        for (Team team : getTeams().values()) {
+            team.getTaxi().removeBodyFromWorld(getWorld());
+        }
+        this.teams.clear();
+        timer = new CountDownTimer(300);
+        System.out.println("resetting done!");
 
-	}
+    }
 
-	public boolean isActive() {
-		return isActive;
-	}
+    public boolean isActive() {
+        return isActive;
+    }
 
-	public void setActive(boolean isActive) {
-		this.isActive = isActive;
-	}
-	
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
 }
--- Implementation/taxi_trouble/core/src/com/taxi_trouble/game/screens/ViewObserver.java
+++ Implementation/taxi_trouble/core/src/com/taxi_trouble/game/screens/ViewObserver.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.Timer.Task;
 import com.taxi_trouble.game.model.CountDownTimer;
 import com.taxi_trouble.game.model.GameWorld;
@@ -29,136 +30,149 @@ import com.taxi_trouble.game.screens.hud.TimerHUD;
  * @author Computer Games Project Group 8
  */
 public abstract class ViewObserver implements Screen {
-	protected final GameWorld taxigame;
-	protected Team team;
-	protected Taxi ownTaxi;
-	protected WorldMap cityMap;
-	protected OrthographicCamera hudCamera;
-	protected HeadUpDisplay hud;
-	protected TimerHUD dropOffTimerHUD;
+    protected final GameWorld taxigame;
+    protected Team team;
+    protected Taxi ownTaxi;
+    protected WorldMap cityMap;
+    protected OrthographicCamera hudCamera;
+    protected HeadUpDisplay hud;
+    protected TimerHUD dropOffTimerHUD;
+    private boolean alreadyResetted = false;
 
-	/**
-	 * Constructor for creating game Screen.
-	 * 
-	 * @param taxigame
-	 */
-	public ViewObserver(GameWorld taxiGame) {
-		this.taxigame = taxiGame;
-	}
+    /**
+     * Constructor for creating game Screen.
+     * 
+     * @param taxigame
+     */
+    public ViewObserver(GameWorld taxiGame) {
+        this.taxigame = taxiGame;
+    }
 
-	/**
-	 * Called when the screen is set as current screen.
-	 * 
-	 */
-	@Override
-	public void show() {
-		this.team = taxigame.getTeam();
-		this.ownTaxi = taxigame.getTeam().getTaxi();
-		this.cityMap = taxigame.getMap();
-		this.hudCamera = new OrthographicCamera();
-		this.hudCamera.setToOrtho(false, BUTTON_CAM_WIDTH, BUTTON_CAM_HEIGHT);
-		this.initializeHUD();
-		taxigame.getTimer().setEndCountDownEvent(new Task() {
-			@Override
-			public void run() {
-				taxigame.getMultiplayerInterface().submitScore(team.getScore());
-				if (taxigame.getMultiplayerInterface().isHost()) {
-					Team winner = taxigame.getWinner();
-					taxigame.getMultiplayerInterface().sendEndMessage(winner);
-					showEndResultsBoard(winner);
-					taxigame.setActive(false);
-				}
-			}
-		});
-	}
+    /**
+     * Called when the screen is set as current screen.
+     * 
+     */
+    @Override
+    public void show() {
+        this.team = taxigame.getTeam();
+        this.ownTaxi = taxigame.getTeam().getTaxi();
+        this.cityMap = taxigame.getMap();
+        this.hudCamera = new OrthographicCamera();
+        this.hudCamera.setToOrtho(false, BUTTON_CAM_WIDTH, BUTTON_CAM_HEIGHT);
+        this.initializeHUD();
+        taxigame.getTimer().setEndCountDownEvent(new Task() {
+            @Override
+            public void run() {
+                taxigame.getMultiplayerInterface().submitScore(team.getScore());
+                if (taxigame.getMultiplayerInterface().isHost()) {
+                    Team winner = taxigame.getWinner();
+                    taxigame.getMultiplayerInterface().sendEndMessage(winner);
+                    showEndResultsBoard(winner);
+                    taxigame.setActive(false);
+                }
+            }
+        });
+    }
 
-	/**
-	 * Displays the winner of the game at the end.
-	 * 
-	 */
-	public void showEndResultsBoard(Team winner) {
-		this.hud.removeAll();
-		HUDComponent endGameHud = new EndGameHUD(winner, 340, 300);
-		this.hud.add(endGameHud);
-	}
+    /**
+     * Displays the winner of the game at the end.
+     * 
+     */
+    public void showEndResultsBoard(Team winner) {
+        this.hud.removeAll();
+        HUDComponent endGameHud = new EndGameHUD(winner, 340, 300);
+        this.hud.add(endGameHud);
+    }
 
-	/**
-	 * Update the world and draw the sprites of the world.
-	 * 
-	 * @param delta
-	 *            delta to be rendered.
-	 */
-	@Override
-	public void render(float delta) {
-		if (!taxigame.isActive()){
-			taxigame.reset();
-			return;
-		}
-		// Initializes new and removes old entity bodies into/from the world
-		taxigame.getSpawner().updateEntityBodies(taxigame.getWorld());
+    /**
+     * Update the world and draw the sprites of the world.
+     * 
+     * @param delta
+     *            delta to be rendered.
+     */
+    @Override
+    public void render(float delta) {
+        checkReset();
+        // Initializes new and removes old entity bodies into/from the world
+        taxigame.getSpawner().updateEntityBodies(taxigame.getWorld());
 
-		// Progress the physics of the game
-		taxigame.getWorld().step(Gdx.app.getGraphics().getDeltaTime(), 3, 3);
+        // Progress the physics of the game
+        taxigame.getWorld().step(Gdx.app.getGraphics().getDeltaTime(), 3, 3);
 
-		// Update the taxi movement for all taxis and render their sprites
-		for (Team team : taxigame.getTeams().values()) {
-			Taxi taxi = team.getTaxi();
-			taxi.update(Gdx.app.getGraphics().getDeltaTime());
-			taxi.render(getSpriteBatch());
-		}
+        // Update the taxi movement for all taxis and render their sprites
+        for (Team team : taxigame.getTeams().values()) {
+            Taxi taxi = team.getTaxi();
+            taxi.update(Gdx.app.getGraphics().getDeltaTime());
+            taxi.render(getSpriteBatch());
+        }
 
-		// Render the passengers into the game
-		for (Passenger pass : taxigame.getPassengers()) {
-			pass.render(getSpriteBatch());
-		}
+        // Render the passengers into the game
+        for (Passenger pass : taxigame.getPassengers()) {
+            pass.render(getSpriteBatch());
+        }
 
-		// Show the destination for a taxi picking up the corresponding
-		// passenger
-		if (ownTaxi.pickedUpPassenger()) {
-			ownTaxi.getPassenger().getDestination().render(getSpriteBatch());
-			showDropOffTimer(ownTaxi.getPassenger().getDropOffTimer());
-		} else {
-			hideDropOffTimer();
-		}
+        // Show the destination for a taxi picking up the corresponding
+        // passenger
+        if (ownTaxi.pickedUpPassenger()) {
+            ownTaxi.getPassenger().getDestination().render(getSpriteBatch());
+            showDropOffTimer(ownTaxi.getPassenger().getDropOffTimer());
+        } else {
+            hideDropOffTimer();
+        }
 
-		// Render the power-ups into the game
-		for (PowerUp pow : taxigame.getPowerUps()) {
-			if (!pow.isTaken()) {
-				pow.render(getSpriteBatch());
-			}
-		}
-		getSpriteBatch().setProjectionMatrix(hudCamera.combined);
-		this.hud.render(getSpriteBatch());
-	}
+        // Render the power-ups into the game
+        for (PowerUp pow : taxigame.getPowerUps()) {
+            if (!pow.isTaken()) {
+                pow.render(getSpriteBatch());
+            }
+        }
+        getSpriteBatch().setProjectionMatrix(hudCamera.combined);
+        this.hud.render(getSpriteBatch());
+    }
 
-	private void showDropOffTimer(CountDownTimer dropOffTimer) {
-		if (!this.hud.contains(dropOffTimerHUD)) {
-			dropOffTimerHUD = new TimerHUD("Drop-off time-limit:", 250, 100,
-					dropOffTimer);
-			this.hud.add(dropOffTimerHUD);
-		}
-	}
+    private void showDropOffTimer(CountDownTimer dropOffTimer) {
+        if (!this.hud.contains(dropOffTimerHUD)) {
+            dropOffTimerHUD = new TimerHUD("Drop-off time-limit:", 250, 100,
+                    dropOffTimer);
+            this.hud.add(dropOffTimerHUD);
+        }
+    }
 
-	private void hideDropOffTimer() {
-		this.hud.remove(this.dropOffTimerHUD);
-	}
+    private void hideDropOffTimer() {
+        this.hud.remove(this.dropOffTimerHUD);
+    }
 
-	/**
-	 * Initializes the HUD that should be displayed on screen together with all
-	 * components.
-	 */
-	private void initializeHUD() {
-		this.hud = new HeadUpDisplay(getHudFont(), team);
-		this.hud.add(new TeamHUD("Team", 10, 470));
-		this.hud.add(new ScoreHUD("Score:", 10, 440));
-		this.hud.add(new TimerHUD("Time:", 680, 470, taxigame.getTimer()));
-	}
+    /**
+     * Initializes the HUD that should be displayed on screen together with all
+     * components.
+     */
+    private void initializeHUD() {
+        this.hud = new HeadUpDisplay(getHudFont(), team);
+        this.hud.add(new TeamHUD("Team", 10, 470));
+        this.hud.add(new ScoreHUD("Score:", 10, 440));
+        this.hud.add(new TimerHUD("Time:", 680, 470, taxigame.getTimer()));
+    }
 
-	/**
-	 * Retrieve the spriteBatch that should be used.
-	 * 
-	 * @return spriteBatch
-	 */
-	public abstract SpriteBatch getSpriteBatch();
+    /**
+     * Retrieve the spriteBatch that should be used.
+     * 
+     * @return spriteBatch
+     */
+    public abstract SpriteBatch getSpriteBatch();
+
+    /**
+     * Checks whether the game is active and reset it.
+     */
+    private void checkReset() {
+        if (!taxigame.isActive() && !alreadyResetted) {
+            alreadyResetted = true;
+            Timer.schedule(new Task() {
+                @Override
+                public void run() {
+                    taxigame.reset();
+                }
+            }, 5);
+        }
+    }
 
 }
